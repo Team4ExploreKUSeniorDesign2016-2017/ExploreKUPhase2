11ff69ae66bae5cada792135aa2d346a5ece43dd
Hung Nguyen <hungtn95@gmail.com>
2017-02-26 22:54:05 -0600

    Add location API

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,6 +2,13 @@ class Api::V1::LocationsController < ApplicationController
   respond_to :json
 
   def show
-    respond_with Location.find(params[:id])
+    location = Location.find(params[:id])
+    locatable = location.locatable.as_json(:except => [:created_at, :updated_at])
+    if location.locatable_type == "Building"
+      amenities = location.locatable.amenities.as_json(:except => [:building_id, :created_at, :updated_at])
+      departments = location.locatable.departments.as_json(:except => [:building_id, :created_at, :updated_at])
+      locatable = locatable.merge(amenities: amenities, departments: departments)
+    end
+    respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable) 
   end
 end
