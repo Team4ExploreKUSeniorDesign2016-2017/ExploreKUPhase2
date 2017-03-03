4a29e2661a806368a5ba3a2d35505bf22e97338c
Hung Nguyen <hungtn95@gmail.com>
2017-03-03 16:44:47 -0600

    Fix something

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -16,7 +16,6 @@ class Api::V1::LocationsController < ApplicationController
       amenities = location.locatable.amenities.as_json(:except => [:building_id, :created_at, :updated_at])
       departments = location.locatable.departments.as_json(:except => [:building_id, :created_at, :updated_at])
       locatable = locatable.merge(amenities: amenities, departments: departments)
-      respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable)
     end
     respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable)
   end
