9bde23ad7d95f1dda5b2eea2124fc89006667ec1
y474z260 <yangzhou@Yang-ZhoumatoMacBook-Pro.local>
2017-03-03 13:21:42 -0600

    API, Buildings' altitude added
    Added a new column for buildings altitudes.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -8,7 +8,13 @@ class Api::V1::LocationsController < ApplicationController
       amenities = location.locatable.amenities.as_json(:except => [:building_id, :created_at, :updated_at])
       departments = location.locatable.departments.as_json(:except => [:building_id, :created_at, :updated_at])
       locatable = locatable.merge(amenities: amenities, departments: departments)
+      respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable) 
+    elsif location.locatable_type == "ParkingLot"
+       respond_with location.as_json(:except => [:lot_id, :created_at, :updated_at]).merge(locatable: locatable)
+    else location.locatable_type == "BusStop"
+      routes = location.locatable.routes.as_json(:except => [:bus_id, :created_at, :updated_at])
+      locatable = locatable.merge(routes: routes)
+      respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at, :routeAssignments]).merge(locatable: locatable)
     end
-    respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable) 
   end
 end
