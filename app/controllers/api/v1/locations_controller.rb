25dabcac9216ad1dde8d9e7221f2558e7bfb501d
Hung Nguyen <hungtn95@gmail.com>
2017-03-03 16:23:39 -0600

    Change JSON format

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::LocationsController < ApplicationController
     lat = params[:lat]
     lng = params[:lng]
     distance = params[:distance]
-    respond_with Location.proximity(lat, lng, distance)
+    respond_with Location.proximity(lat, lng, distance).as_json(:except => [:locatable_id, :locatable, :created_at, :updated_at])
     # ?lat=38.960401&lng=-95.242294&distance=2
   end
 
@@ -16,13 +16,8 @@ class Api::V1::LocationsController < ApplicationController
       amenities = location.locatable.amenities.as_json(:except => [:building_id, :created_at, :updated_at])
       departments = location.locatable.departments.as_json(:except => [:building_id, :created_at, :updated_at])
       locatable = locatable.merge(amenities: amenities, departments: departments)
-      respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable) 
-    elsif location.locatable_type == "ParkingLot"
-       respond_with location.as_json(:except => [:lot_id, :created_at, :updated_at]).merge(locatable: locatable)
-    else location.locatable_type == "BusStop"
-      routes = location.locatable.routes.as_json(:except => [:bus_id, :created_at, :updated_at])
-      locatable = locatable.merge(routes: routes)
-      respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at, :routeAssignments]).merge(locatable: locatable)
+      respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable)
     end
+    respond_with location.as_json(:except => [:locatable_id, :created_at, :updated_at]).merge(locatable: locatable)
   end
 end
